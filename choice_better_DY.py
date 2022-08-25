8e7ff7b07529ad6b7ea53470e07764871f2528af
Rafael Cassau <rafael.cassau@loadsmart.com>
2022-08-25 11:47:11 -0300

    make ordering by parameter more flexible:

--- choice_better_DY.py
+++ choice_better_DY.py
@@ -10,7 +10,6 @@ FIIS_LIST = [
     ("KNRI11", "Laje/Logistica"),
     ("XPLG11", "Logistica"),
     ("VISC11", "Shopping"),
-    ("XPPR11", "Laje comercial"),
     ("VRTA11", "CRI"),
     ("MXRF11", "CRI"),
     ("XPML11", "Shopping"),
@@ -19,6 +18,8 @@ FIIS_LIST = [
     ("IRDM11", "CRI"),
     ("PVBI11", "Laje comercial"),
     ("BTLG11", "Logistica"),
+    ("HGLG11", "Logistica"),
+    ("HGRU11", "Renda Urbana"),
 ]
 
 URL_PREFIX = "https://www.fundsexplorer.com.br/funds/{}"
@@ -56,8 +57,8 @@ def to_decimal(value):
     return Decimal(value)
 
 
-def find_most_high_yield_return_based_on_last_payment(money, fiis_list):
-    next_payment_yield_list = []
+def build_fiis_list(money, fiis_list):
+    fiis_new_list = []
     for fii in fiis_list:
         kind = fii["kind"]
         market_value = fii["indicators"]["market_value"]
@@ -66,7 +67,7 @@ def find_most_high_yield_return_based_on_last_payment(money, fiis_list):
         quotas_amount = money // market_value
         change = money % market_value
         next_payment_yield_BRL = quotas_amount * last_yield
-        next_payment_yield_list.append({
+        fiis_new_list.append({
             "fii": fii["fii"],
             "kind": kind,
             "quotas_amount": quotas_amount,
@@ -75,9 +76,12 @@ def find_most_high_yield_return_based_on_last_payment(money, fiis_list):
             "equity_value": equity_value,
             "market_value": market_value,
             "P/VP": fii["indicators"]["P/VP"]
-        })  
+        })
+    return fiis_new_list
 
-    return sorted(next_payment_yield_list, key=lambda fii: fii["next_payment_yield_BRL"], reverse=True)
+
+def ordering_fiis_by(fiis_list, order_by, reverse=False):
+    return sorted(fiis_list, key=lambda fii: fii[order_by], reverse=reverse)
 
 
 def print_fiis(fiis_list):
@@ -94,12 +98,15 @@ def print_fiis(fiis_list):
 
 
 if __name__ == "__main__":
-    fiis_data_list = scrapy_fiis_price(FIIS_LIST)
-
     money = Decimal(input("informe o valor do aporte em R$: "))
 
-    next_payment_yield_list = find_most_high_yield_return_based_on_last_payment(money, fiis_data_list)
-    print_fiis(next_payment_yield_list)
+    fiis_data_list = scrapy_fiis_price(FIIS_LIST)
+    fiis = build_fiis_list(money, fiis_data_list)
+
+    #ordered_fiis = ordering_fiis_by(fiis, "next_payment_yield_BRL", reverse=True)
+    ordered_fiis = ordering_fiis_by(fiis, "P/VP", reverse=False)
+
+    print_fiis(ordered_fiis)
 
 
 
